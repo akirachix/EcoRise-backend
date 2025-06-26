c53150a6a4c32214338641dccbf8046a3498f60a
Faith Koduen <faithkoduen@gmail.com>
2025-06-26 15:29:03 +0300

    added the product app

---
+++ Ecorise/product/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Product(models.Model):
+   product_id = models.AutoField(primary_key=True)
+   quantity = models.IntegerField()
+   type = models.CharField(max_length=50)
+   price = models.IntegerField()
+   listed_at = models.DateTimeField()
+   def __str__(self):
+       return f"{self.type} (ID: {self.product_id})"
+
+
+
+class Material(models.Model):
+    material_id = models.AutoField(primary_key=True)
+    material_type = models.CharField(max_length=50, unique=True)
+    price_per_kg = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.material_type} ({self.material_id})"
