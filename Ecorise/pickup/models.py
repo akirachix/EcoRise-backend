bb0d47de8e964561c278142d4367e42f74e44652
student <oumagladwineesther@gmail.com>
2025-06-29 21:33:43 +0300

    Created an pickup model

---
+++ Ecorise/pickup/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+# from product.models import Material
+# from users.models import User
+
+class Pickup(models.Model):
+    request_id = models.AutoField(primary_key=True)
+    # material = models.ForeignKey(Material, on_delete =models.CASCADE)
+    # trader = models.ForeignKey(User, on_delete = models.CASCADE)
+    trader_name = models.CharField(max_length = 255)
+    phone_number = models.IntegerField(unique = True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    latitude = models.DecimalField(max_digits = 9, decimal_places = 6)
+    longitude = models.DecimalField(max_digits = 9, decimal_places = 6)
+    pickup_status = models.CharField(max_length = 50, choices = [("Pending", "Pending"), ("Confirmed", "Confirmed")])
+    pickup_at = models.DateTimeField(null = True, blank = True)
+   
+    def __str__(self):
+        return f"{self.request_id }"
