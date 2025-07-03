7b47f855952debe4219e88efd505678662bc83d0
student <oumagladwineesther@gmail.com>
2025-07-03 17:23:19 +0300

    Updated the picku modelp

--- Ecorise/pickup/models.py
+++ Ecorise/pickup/models.py
@@ -5,8 +5,7 @@ from users.models import User
 class Pickup(models.Model):
     request_id = models.AutoField(primary_key=True)
     material = models.ForeignKey(Material, on_delete =models.CASCADE)
-    trader_id = models.ForeignKey(User, on_delete = models.CASCADE)
-    recycler_id = models.ForeignKey(User, on_delete = models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add = True)
     market_location = models.CharField(max_length = 100)
     market_latitude = models.DecimalField(max_digits = 9, decimal_places = 6)
@@ -16,3 +15,4 @@ class Pickup(models.Model):
    
     def __str__(self):
         return f"{self.request_id }"
+
