855456b0f4888825d5447081e0bb9d7991c55c18
student <oumagladwineesther@gmail.com>
2025-07-03 15:21:29 +0300

    Updated class name in feedback and pickup model

--- Ecorise/pickup/models.py
+++ Ecorise/pickup/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-from product.models import Material
+from material.models import Material
 from users.models import User
 
 class Pickup(models.Model):
     request_id = models.AutoField(primary_key=True)
     material = models.ForeignKey(Material, on_delete =models.CASCADE)
-    trader = models.ForeignKey(User, on_delete = models.CASCADE)
-    trader_name = models.CharField(max_length = 255)
-    phone_number = models.IntegerField(unique = True)
+    trader_id = models.ForeignKey(User, on_delete = models.CASCADE)
+    recycler_id = models.ForeignKey(User, on_delete= models.CASCADE)
     created_at = models.DateTimeField(auto_now_add = True)
-    latitude = models.DecimalField(max_digits = 9, decimal_places = 6)
-    longitude = models.DecimalField(max_digits = 9, decimal_places = 6)
+    market_location = models.CharField(max_length = 100)
+    market_latitude = models.DecimalField(max_digits = 9, decimal_places = 6)
+    market_longitude = models.DecimalField(max_digits = 9, decimal_places = 6)
     pickup_status = models.CharField(max_length = 50, choices = [("Pending", "Pending"), ("Confirmed", "Confirmed")])
     pickup_at = models.DateTimeField(null = True, blank = True)
    
