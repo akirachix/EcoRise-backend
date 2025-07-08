76afe1126448f0c83607d7c783b1ca588cd5f447
Amuor <mangaramuor@gmail.com>
2025-07-08 14:14:12 +0300

    updated the user_type model

--- Ecorise/users/models.py
+++ Ecorise/users/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 USER_TYPE_CHOICES = [('Trader','trader'),('recycler','Recycler')]
+
 class User(models.Model):
     user_id = models.AutoField(primary_key = True)
     name = models.CharField(max_length = 100)
     email = models.EmailField(max_length = 100,unique = True,null = True,blank = True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
-    user_type = models.CharField(max_length=50, choices=[('recycler', 'Recycler'), ('traders', 'trader')], blank=True, null=True)
+    user_type = models.CharField(max_length=50, choices= USER_TYPE_CHOICES)
     password = models.CharField(max_length = 100)
     created_at = models.DateTimeField(auto_now_add = True)
     
