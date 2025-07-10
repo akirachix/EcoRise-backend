3026bfae490d316922d06a65bf22db448ca640d9
student <oumagladwineesther@gmail.com>
2025-07-10 15:47:39 +0300

    Made migrations

--- Ecorise/feedback/models.py
+++ Ecorise/feedback/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-# from users.model import User
+from users.model import User
 
 
 class Feedback(models.Model):
     feedback_id = models.AutoField(primary_key=True)
-    # user = models.ForeignKey(User, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add = True)
     feedback = models.TextField(max_length = 500)
 
