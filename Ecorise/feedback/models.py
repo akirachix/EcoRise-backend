c91bbd7cdca1074b03ae2bbbc3b45e87f2113941
student <oumagladwineesther@gmail.com>
2025-07-11 17:15:23 +0300

    Added migration files

--- Ecorise/feedback/models.py
+++ Ecorise/feedback/models.py
@@ -1,10 +1,14 @@
 from django.db import models
-from users.model import User
+from users.models import User
 
 
 class Feedback(models.Model):
     feedback_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user_type = models.CharField(max_length=50, default='Trader')
+
+
+
     created_at = models.DateTimeField(auto_now_add = True)
     feedback = models.TextField(max_length = 500)
 
