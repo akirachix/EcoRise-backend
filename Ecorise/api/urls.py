681928f95f1168e92b0c8e303e3dccddef819ad9
Eyerusalem Yrga <eyerusalemykahsay>
2025-07-15 07:21:56 +0300

    create api model

--- Ecorise/api/urls.py
+++ Ecorise/api/urls.py
@@ -1,14 +1,23 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-router=DefaultRouter()
-from .views import PaymentViewSet, RewardViewSet
+from .views import PaymentViewSet, RewardViewSet, b2c_payment, b2c_result, b2c_timeout
+from django.http import HttpResponse
 
-router=DefaultRouter()
-router.register(r'Payment', PaymentViewSet)
-router.register(r'reward', RewardViewSet)
-urlpatterns=[
+def home(request):
+    return HttpResponse("Welcome to Ecorise API", status=200)
+
+
+
+from . import views
+router = DefaultRouter()
+router.register(r'payment', PaymentViewSet, basename='payment')
+router.register(r'reward', RewardViewSet, basename='reward')
+
+urlpatterns = [
     path('', include(router.urls)),
+        path('api/daraja/b2c/', b2c_payment, name='b2c_payment'),
+    path('api/daraja/b2c/result/', b2c_result, name='b2c_result'),
+    path('api/daraja/b2c/timeout/', b2c_timeout, name='b2c_timeout'),
+    path('', home, name='home'),
+    
 ]
-
-
-
