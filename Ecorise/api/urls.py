1ab662356eccc59581392021ef94f9269895a1a7
student <oumagladwineesther@gmail.com>
2025-07-17 18:20:42 +0300

    Modified urls.py

--- Ecorise/api/urls.py
+++ Ecorise/api/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path, include
 from django.http import HttpResponse
 from rest_framework.routers import DefaultRouter
+from feedback.views import FeedbackViewSet
 
 from .views import (
     PickupViewSet, UserViewSet, MaterialViewSet, ProductViewSet,
     PaymentViewSet, RewardViewSet, b2c_payment, b2c_result, b2c_timeout
 )
-from feedback.views import FeedbackViewSet
 
-def home(request):
-    return HttpResponse("Welcome to Ecorise API", status=200)
+
+
 
 router = DefaultRouter()
 router.register(r'pickups', PickupViewSet, basename='pickup')
@@ -25,5 +25,5 @@ urlpatterns = [
     path('api/daraja/b2c/', b2c_payment, name='b2c_payment'),
     path('api/daraja/b2c/result/', b2c_result, name='b2c_result'),
     path('api/daraja/b2c/timeout/', b2c_timeout, name='b2c_timeout'),
-    path('', home, name='home'),
+    
 ]
