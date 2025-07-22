2d8025a59fbf199e05aa44441e142a0ba9bf27c4
student <oumagladwineesther@gmail.com>
2025-07-22 14:35:24 +0300

    updated setting.py and requirements.txt

--- Ecorise/settings.py
+++ Ecorise/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(=&vvaohu)4_g$zeh3c(t_(9f4rswh#3=ga-iy#+-jp=7r8h+d'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
